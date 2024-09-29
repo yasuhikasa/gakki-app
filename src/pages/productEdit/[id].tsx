ca8e9ba497f3c58d645f3768a7481a98a8ae1269
yasuhikasa <yasuhikasa@yahoo.co.jp>
2024-09-29 18:54:17 +0900

    不要な関数削除

--- src/pages/productEdit/[id].tsx
+++ src/pages/productEdit/[id].tsx
@@ -9,16 +9,10 @@ const ProductEditPage: NextPage = () => {
 
   if (!id) return <p>商品IDが見つかりません。</p>;
 
-  // 保存が完了した後に呼ばれるコールバック関数
-  const handleSave = () => {
-    alert('商品が更新されました');
-    router.push('/productList'); // 保存後に商品一覧ページへリダイレクト
-  };
-
   return (
     <div>
       {/* 商品IDがある場合にフォームを表示 */}
-      {typeof id === 'string' && <ProductEditForm productId={id} onSave={handleSave} />}
+      {typeof id === 'string' && <ProductEditForm productId={id} />}
     </div>
   );
 };
